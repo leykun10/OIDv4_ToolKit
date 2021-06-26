e7fd93574289a5dfe37ea5ba43d90b5aa8b85425
unknown <leykunejigue@gmail.com>
2021-06-26 03:09:03 +0300

    conversion finalized

--- convert_to_yolov5_format.py
+++ convert_to_yolov5_format.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 
 def name_to_class(name):
@@ -6,29 +7,36 @@ def name_to_class(name):
         return "0"
     elif name.__eq__("Car"):
         return "1"
-    elif name.__eq__("Vehicle_registration_plate"):
-        return "2"
     elif name.__eq__("Motorcycle"):
+        return "2"
+    elif name.__eq__("Vehicle_registration_plate"):
         return "3"
     else:
         return "no_class"
 
 
-def main():
-    with open('tr.txt', 'r+') as f:
-        arrays = []
-        for line in f:
-            print(line)
-            array = line.split(sep=" ")
-            array[0] = name_to_class(array[0])
+def main(path):
 
-            line = " ".join(array)
-            print(line)
-            arrays.append(line)
-        f.seek(0)
-        f.writelines(arrays)
-        f.truncate()
+    for file in os.listdir(path):
+        data = os.path.join(path, file)
+        with open(data, 'r+') as f:
+            arrays = []
+            for line in f:
+                print(line)
+                array = line.split(sep=" ")
+                array[0] = name_to_class(array[0])
+
+                line = " ".join(array)
+                print(line)
+                arrays.append(line)
+            f.seek(0)
+            f.writelines(arrays)
+            f.truncate()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Path to dataset')
+    parser.add_argument("--path", required=True)
+
+    args = parser.parse_args()
+    main(str(args.path))
